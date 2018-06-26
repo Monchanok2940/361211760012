352626be077a70c5b5fbe58956af0bacbd63aa62
monchanokp76 <monchanokp76@gmail.com>
2018-06-26 12:08:44 +0700

    Hell

---
+++ src/lab1/mycode.java
@@ -0,0 +1,17 @@
+package lab1;
+
+public class mycode {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Monchanok Pinlawas");
+        System.out.println("361211760012");
+        System.out.println("MIT211");
+        System.out.println("RMUTSV");
+    }
+
+
+
+
+}
+
